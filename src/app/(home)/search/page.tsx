f077991b1065552e0b5f96571a0bbd181faefcd5
hussien tawhidi <hussientawhidi710@gmail.com>
2025-07-16 16:13:28 +0330

    update

--- src/app/(home)/search/page.tsx
+++ src/app/(home)/search/page.tsx
@@ -1,15 +1,15 @@
 import { Suspense } from "react";
 import SearchResults from "./SearchResult";
 
-export default function SearchPage({
-  searchParams,
-}: {
-  searchParams: { query?: string };
-}) {
-  const query = searchParams.query ?? "";
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function SearchPage({ searchParams }: any) {
+  // const searchParams = await params;
+  const query = (await searchParams.query) ?? "";
+  console.log("🚀 ~ SearchPage ~ searchParams:", searchParams);
 
   return (
     <div className='max-w-4xl mx-auto px-4 py-8'>
+      <p>{query}</p>
       <h1 className='text-xl font-bold mb-4'>نتایج جستجو برای: {query}</h1>
       <Suspense fallback={<p>در حال بارگذاری...</p>}>
         <SearchResults query={query} />
